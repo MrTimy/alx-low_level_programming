8c594ffb6a68d27f6665cdc606932574d21ae2f3
mrtimy <eketimothy84@gmail.com>
2022-06-02 15:44:42 -0700

    C program that prints the exact word and moves to a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - prints the string in the puts function
+ *
+ * Description: prints "Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+
+}
