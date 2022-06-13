cead8de67e08b1dc87dcb4296ba8e974afab3f9f
mrtimy <eketimothy84@gmail.com>
2022-06-13 12:14:00 -0700

    takes a pointer to an int as parameter and updates the value it points to to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * reset_to_98 - sets value in memory location to 98.
- * @n: parameter
- * Return: void.
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ * @n: pointer to the variable to update
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_FILE
 #define MAIN_FILE
 
+int _putchar(char c);
 
 void reset_to_98(int *n);
 
@@ -20,6 +21,6 @@ void puts_half(char *str);
 
 void print_array(int *a, int n);
 
-char *_strcpy(char *dest, char *src;
-		)
+char *_strcpy(char *dest, char *src);
+
 #endif
