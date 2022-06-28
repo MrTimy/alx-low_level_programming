c358a3fbbbbec631c19d5a863ec41787636557ef
mrtimy <eketimothy84@gmail.com>
2022-06-28 16:20:47 -0700

    returns a pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,52 +1,44 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-* _strlen - Swaps integers wih pointers.
-*
-* @s: is a pointer to a char
-*
-* Return: Always 0.
-*/
-int _strlen(char *s)
+ * alloc_grid - function that returns a pointer to a 2 dimensional array of int
+ *
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: address of the newly allocated memory
+ */
+
+int **alloc_grid(int width, int height)
 {
-int i = 0;
-while (*(s + i) != '\0')
-i++;
-return (i);
-}
-/**
-* str_concat - Concatenates 2 strings..
-*
-* @s1: First string.
-* @s2: Second string.
-*
-* Return: Returns the created array.
-**/
-char *str_concat(char *s1, char *s2)
-{
-int i, j, size1, size2, totSize;
-char *ar;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-size1 = _strlen(s1);
-size2 = _strlen(s2);
-totSize = (size1 + size2)+1;
-ar = malloc(totSize *sizeof(char));
-if (ar == NULL)
-return (NULL);
-else
-{
-for (i = 0; i < size1; i++)
-ar[i] = s1[i];
-for (j = 0; j < size2; j++)
-{
-ar[i] = s2[j];
-i++;
-}
-ar[i + 1] = '\0';
-return (ar);
-}
+	int i, j, m, n;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+	for (m = 0; m < height; m++)
+	{
+		for (n = 0; n < width; n++)
+			grid[m][n] = 0;
+	}
+	return (grid);
 }
